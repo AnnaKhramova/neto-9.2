afc3161090403af8c5172ce2b71cfde81ce25d75
Anna Khramova <a.hramova@astondevs.ru>
2024-06-26 09:51:52 +0300

    Fix bean inject

--- src/main/java/ru/akhramova/task_9_2/model/User.java
+++ src/main/java/ru/akhramova/task_9_2/model/User.java
@@ -7,12 +7,13 @@ import jakarta.validation.constraints.Size;
 import java.util.Objects;
 
 public class User {
-    @NotBlank
-    @Size(min = 2)
+    @NotBlank(message = "Имя должно состоять хотя бы из двух букв")
+    @Size(min = 2, message = "Имя должно состоять хотя бы из двух букв")
+    @Pattern(regexp = "[a-zA-Z]{2,}", message = "Имя должно состоять из букв")
     private String user;
-    @NotBlank
-    @Size(min = 5)
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}")
+    @NotBlank(message = "Пароль должен состоять хотя бы из шести символов")
+    @Size(min = 6, message = "Пароль должен состоять хотя бы из шести символов")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}", message = "Пароль должен содержать строчные и заглавные буквы, цифры и спец. символы")
     private String password;
 
     public User(String user, String password) {
@@ -31,5 +32,11 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(user, password);
+    }
+
+    @Override
+    public String toString() {
+        return "user='" + user + '\'' +
+                ", password='" + password;
     }
 }
